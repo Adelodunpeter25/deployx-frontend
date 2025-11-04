6a6412e8704e244cb08c6da2a72a27cba7ae7a8e
Peter Adelodun <adelodunpeter69@gmail.com>
2025-11-04 17:19:17 +0100

    removed testimonials section in the landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,6 @@
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
-import Testimonials from '../components/Testimonials'
 import Footer from '../components/Footer'
 
 export default function Landing() {
@@ -10,7 +9,6 @@ export default function Landing() {
       <Header />
       <Hero />
       <Features />
-      <Testimonials />
       <Footer />
     </div>
   )
